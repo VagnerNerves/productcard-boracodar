7f0211480c921822d8c59a75ff74bd457d1f9e19
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-12 16:05:48 -0300

    feat: Ajusted color pattern and font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+      serif: ['Crimson Pro', 'serif']
+    },
+    extend: {
+      colors: {
+        fuchsia: {
+          200: '#D9CDF7',
+          900: '#271A45'
+        }
+      }
+    }
   },
   plugins: []
 }
